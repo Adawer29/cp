5090c9aaae5038e8a8d76c8100bd71e41709e8a0
Karan <karangupta199920@gmail.com>
2019-09-10 12:04:18 +0530

    levels app migration fixed

--- campus/levels/models.py
+++ campus/levels/models.py
@@ -1,25 +1,26 @@
+from django import template
 from django.db import models
 from django.contrib.auth import get_user_model
 from PIL import Image
 from django.core.files.storage import FileSystemStorage
-User=get_user_model()
-# Create your models here.
-from django import template
-register=template.Library()
+
+
 class Answer(models.Model):
-    name=models.CharField(max_length=10,unique=True)
-    answer=models.CharField(max_length=100)
+    name = models.CharField(max_length=10, unique=True)
+    answer = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        ordering= ["-name"]
+        ordering = ["-name"]
+
 
 class Question(models.Model):
-    question_relation=models.ForeignKey(Answer,related_name='ques',on_delete=models.CASCADE)
-    question_image=models.ImageField(upload_to='levels/')
-    text=models.TextField(blank=True,max_length=100)
+    question_relation = models.ForeignKey(
+        Answer, related_name='ques', on_delete=models.CASCADE)
+    question_image = models.ImageField(upload_to='levels/')
+    text = models.TextField(blank=True, max_length=100)
 
     def __str__(self):
         return str(self.question_relation)
